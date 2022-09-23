3a653849668f350e7a78d23b133466d6f263acd0
Matota <72810000+mat0ta@users.noreply.github.com>
2022-09-23 17:26:28 +0200

    Cambiado main

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import range
 import tabla
 
 if __name__ == "__main__":
-    # sum.Sum().sum()
+    sum.Sum().sum()
 
-    # string = string.String()
-    # string.checkString()
+    string = string.String()
+    string.checkString()
 
-    # range1 = range.Range(-10, 0)
-    # range1.generate()
-    # range2 = range.Range(0, 50, True, 5)
-    # range2.generate()
-    # range3 = range.Range(0, 10)
-    # range3.generate()
+    range1 = range.Range(-10, 0)
+    range1.generate()
+    range2 = range.Range(0, 50, True, 5)
+    range2.generate()
+    range3 = range.Range(0, 10)
+    range3.generate()
 
     print(tabla.Tabla().tabla())
